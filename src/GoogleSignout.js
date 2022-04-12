9a7a9d8b164e0a9f91f4482a5ea71113178456c8
evansavage <esav678@gmail.com>
2022-04-11 23:13:16 -0400

    Added google sign out component

---
+++ src/GoogleSignout.js
@@ -0,0 +1,24 @@
+import { Button } from "@material-ui/core"
+
+import React from 'react';
+import { GoogleLogout } from 'react-google-login';
+
+
+
+export default function GoogleSignout(props) {
+  const {setAccessToken} = props;
+
+  const logout = (response) => {
+    console.log(response);
+    setAccessToken(null);
+    localStorage.setItem('accessToken', null);
+  }
+
+  return (
+    <GoogleLogout
+      clientId={props.clientID}
+      buttonText="Logout"
+      onLogoutSuccess={logout}
+    />
+  )
+}
