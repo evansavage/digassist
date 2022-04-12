22138e26a4939d5166b4d400315f75fadca8ba7e
evansavage <esav678@gmail.com>
2022-04-11 23:34:02 -0400

    initial useeffect for checking login state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
-import GoogleSignin from './GoogleSignin.js'
-import GoogleSignout from './GoogleSignout.js'
-import { useEffect, useState } from "react"
+import GoogleSignin from './GoogleSignin.js';
+import GoogleSignout from './GoogleSignout.js';
+import { useEffect, useState } from "react";
+// import { useGoogleApi } from 'react-gapi';
+
 
 function App() {
   const [accessToken, setAccessToken] = useState(localStorage.getItem('accessToken', null));
-  const [refreshToken, setRefreshToken] = useState(localStorage.getItem('refreshTokenKey', null));
   const clientID = "588125041912-oo4jcirn1oecot9q9dfm10o0ghal2v5a.apps.googleusercontent.com";
+
+  useEffect(() => {
+    if (accessToken) {
+      console.log('yuh');
+
+    }
+  }, [accessToken])
 
 
   return (
@@ -18,7 +25,7 @@ function App() {
         </p>
         <div>{accessToken}</div>
         {accessToken !== null
-          ? <GoogleSignout clientID={clientID} setAccessToken={setAccessToken} />
+          ? <GoogleSignout clientID={clientID} accessToken={accessToken} setAccessToken={setAccessToken} />
           : <GoogleSignin clientID={clientID} setAccessToken={setAccessToken} />
         }
 
