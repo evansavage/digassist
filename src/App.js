c39d0c483c6f388b57047d69d125f54f09e370a7
evansavage <esav678@gmail.com>
2022-04-11 23:13:45 -0400

    Incorporated the sign in state functionality

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import GoogleSignin from './GoogleSignin.js'
+import GoogleSignout from './GoogleSignout.js'
+import { useEffect, useState } from "react"
 
 function App() {
+  const [accessToken, setAccessToken] = useState(localStorage.getItem('accessToken', null));
+  const [refreshToken, setRefreshToken] = useState(localStorage.getItem('refreshTokenKey', null));
+  const clientID = "588125041912-oo4jcirn1oecot9q9dfm10o0ghal2v5a.apps.googleusercontent.com";
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          DigAssist
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Bruh
-        </a>
+        <div>{accessToken}</div>
+        {accessToken !== null
+          ? <GoogleSignout clientID={clientID} setAccessToken={setAccessToken} />
+          : <GoogleSignin clientID={clientID} setAccessToken={setAccessToken} />
+        }
+
       </header>
     </div>
   );
